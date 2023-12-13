31dfda2f274c668a742e2e23b2fd788dbb3f39dc
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-12-13 06:50:48 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import MoviesList from "./components/Movieslist";
+import NavBar from "./components/NavBar";
 
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movieslist" element={<MoviesList />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
